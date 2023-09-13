c396e2d3a5a869119b3c9e54dddfa531fd7adb40
GasanaJr <gasanadj2003@gmail.com>
2023-09-13 14:08:57 +0200

    feat(webgi): add webgi
    - adds webgi viewer and it's configurations
    - adds threeD models

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import SoundSection from "./components/SoundSection";
 import DisplaySection from "./components/DisplaySection";
+import WebgiViewer from "./components/WebgiViewer";
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
       <Jumbotron />
       <SoundSection />
       <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
