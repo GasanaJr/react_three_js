a33fa5f53311fd56caa95d037b466742e5522848
GasanaJr <gasanadj2003@gmail.com>
2023-09-13 13:32:55 +0200

    feat(sound_section): add the sound section
    - add sound section
    - add scroll effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
+import SoundSection from "./components/SoundSection";
 function App() {
   return (
     <div className="App">
       <Nav />
       <Jumbotron />
+      <SoundSection />
     </div>
   );
 }
--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -3,6 +3,14 @@ import Iphone from "../assets/images/iphone-14.jpg";
 import HoldingIphone from "../assets/images/iphone-hand.png";
 
 const Jumbotron = () => {
+  const handleLearnMore = () => {
+    const element = document.querySelector(".sound-section");
+    window.scrollTo({
+      top: element?.getBoundingClientRect().top,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="jumbotron-section wrapper">
       <h2 className="title">New</h2>
@@ -16,7 +24,9 @@ const Jumbotron = () => {
           <button className="button">Buy</button>
         </li>
         <li>
-          <a className="link">Learn more</a>
+          <a className="link" onClick={handleLearnMore}>
+            Learn more
+          </a>
         </li>
       </ul>
       <img src={HoldingIphone} className="iphone-img" alt="iPhone" />
