430af6fde42a32f9af6f9bece053dab39033d613
GasanaJr <gasanadj2003@gmail.com>
2023-09-13 14:36:23 +0200

    feat(animations): add base animation file

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -20,8 +20,15 @@ import {
 } from "webgi";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { scrollAnimation } from "../lib/scroll-animation";
+gsap.registerPlugin(ScrollTrigger);
 const WebgiViewer = () => {
   const canvasRef = useRef(null);
+  const memoizedScrollAnimation = useCallback((position, target, onUpdate) => {
+    if (position && target && onUpdate) {
+      scrollAnimation(position, target, onUpdate);
+    }
+  }, []);
   const setupViewer = useCallback(async () => {
     // Initialize the viewer
     const viewer = new ViewerApp({
@@ -53,12 +60,18 @@ const WebgiViewer = () => {
     window.scrollTo(0, 0);
 
     let needsUpdate = true;
+    const onUpdate = () => {
+      needsUpdate = true;
+      viewer.setDirty();
+    };
     viewer.addEventListener("preFrame", () => {
       if (needsUpdate) {
         camera.positionTargetUpdated(true);
         needsUpdate = false;
       }
     });
+
+    memoizedScrollAnimation(position, target, onUpdate);
   }, []);
 
   useEffect(() => {
---
+++ src/lib/scroll-animation.js
@@ -0,0 +1,51 @@
+import gsap from "gsap";
+
+export const scrollAnimation = (postion, target, onUpdate) => {
+  const tl = gsap.timeline();
+
+  tl.to(postion, {
+    x: -3.38,
+    y: -10.74,
+    z: -5.93,
+    scrollTrigger: {
+      trigger: ".sound-section",
+      start: "top bottom",
+      end: "top top",
+      scrub: 2,
+      immediateRender: false,
+    },
+    onUpdate,
+  })
+    .to(target, {
+      x: 1.52,
+      y: 0.77,
+      z: -1.08,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+    })
+    .to(".jumbotron-section", {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+    })
+    .to(".sound-section-content", {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: ".sound-section",
+        start: "top bottom",
+        end: "top top",
+        scrub: 2,
+        immediateRender: false,
+      },
+    });
+};
